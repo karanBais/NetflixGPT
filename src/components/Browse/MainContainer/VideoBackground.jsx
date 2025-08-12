35950f63d68f4864d19ea7c9afb472c9869f05c5
KaranBais <karanb479@gmail.com>
2025-08-12 13:50:38 +0530

    VideoBackground changed

--- src/components/Browse/MainContainer/VideoBackground.jsx
+++ src/components/Browse/MainContainer/VideoBackground.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { useSelector } from "react-redux";
 import useMovieTrailer from "../../Hooks/useMovieTrailer";
 import { IMG_API_URL } from "../../../Constants";
@@ -13,12 +13,22 @@ const VideoBackground = ({ movieId }) => {
     ? `${IMG_API_URL}${movieDetails.backdrop_path}`
     : null;
 
+    useEffect(() => {
+  if (window.YT && trailerVideo?.key) {
+    const player = new window.YT.Player("trailer-player", {
+      events: {
+        onReady: (event) => event.target.playVideo(),
+      },
+    });
+  }
+}, [trailerVideo]);
+
   return (
     <div className="absolute top-0 left-0 w-full h-screen overflow-hidden z-0">
       {trailerVideo?.key ? (
         <iframe
           className="w-screen aspect-video object-cover pointer-events-none"
-          src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${trailerVideo?.key}`}
+          src={`https://www.youtube.com/embed/${trailerVideo?.key}?mute=1&autoplay=1&controls=0&loop=1&playlist=${trailerVideo?.key}&enablejsapi=1`}
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
